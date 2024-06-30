eca63d6622fe5ebc0e54b6ec6ae8ecc2b25bee24
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-06-30 02:11:03 +0000

    header

--- main.ts
+++ main.ts
@@ -4,12 +4,7 @@ const app = new Hono()
 
 app.get('/', async (c) => {
   const res = await fetch('https://x.com/nakasyou0', {
-    headers: {
-      accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-      'accept-encoding': c.req.header('accept-encoding') ?? '',
-      'accept-language': c.req.header('accept-language') ?? 'ja',
-      'user-agent': c.req.header('User-Agent') ?? ''
-    },
+    headers: c.req.header(),
   })
   const matched = (await res.text()).match(/https:\/\/twitter\.com\/x\/migrate\?tok=[0-9a-f]+/)
   if (!matched) {
