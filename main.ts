85288d03dee9ef4f177d3008126311d169f9576a
Shotaro Nakamura <syoutarou1103@outlook.jp>
2024-06-30 02:00:34 +0000

    fix: headers

--- main.ts
+++ main.ts
@@ -6,10 +6,9 @@ app.get('/', async (c) => {
   const res = await fetch('https://x.com/nakasyou0', {
     headers: {
       accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-      'accept-encoding': 'gzip, deflate, br',
-      'accept-language': 'ja',
-      'user-agent':
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2 Safari/605.1.15',
+      'accept-encoding': c.req.header('accept-encoding') ?? '',
+      'accept-language': c.req.header('accept-language') ?? 'ja',
+      'user-agent': c.req.header('User-Agent') ?? ''
     },
   })
   const matched = (await res.text()).match(/https:\/\/twitter\.com\/x\/migrate\?tok=[0-9a-f]+/)
